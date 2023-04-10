79a880d3f4e7e65a573cfd536fb99c41c90fab75
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2023-04-11 00:28:10 +0200

    cleanup

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -133,6 +133,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
     use "lukas-reineke/indent-blankline.nvim"
     use({
         'rose-pine/neovim',
