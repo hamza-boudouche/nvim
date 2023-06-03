0aad298f7fcfffc6a5766c2546f1435a29a2d3f3
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2023-06-03 23:27:10 +0200

    added key binding for floating error windows and line wrap for trouble filetype

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,39 @@
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
+
+vim.keymap.set('n', '<leader>;', function()
+    -- If we find a floating window, close it.
+    local found_float = false
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_config(win).relative ~= '' then
+            vim.api.nvim_win_close(win, true)
+            found_float = true
+        end
+    end
+
+    if found_float then
+        return
+    end
+
+    vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
+end, { desc = 'Toggle Diagnostics' })
+
+function enableLineWrapForTrouble()
+    -- Check if the current buffer's file type is "Trouble"
+    if vim.bo.filetype == "Trouble" then
+        -- Enable line wrap
+        vim.wo.wrap = true
+    else
+        -- Disable line wrap
+        vim.wo.wrap = false
+    end
+end
+
+-- Set up an autocmd to trigger the function when a buffer is loaded
+vim.cmd([[
+augroup LineWrapForTrouble
+    autocmd!
+    autocmd BufEnter * lua enableLineWrapForTrouble()
+augroup END
+]])
